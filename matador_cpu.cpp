0d3e45ed551d250ead9ab0539829da24b1bc4867
deyvisondantas <41004665+deyvisondantas@users.noreply.github.com>
2019-04-11 17:32:41 -0300

    matador_cpu.cpp
    Acrescentando a opção de alocar um processo a uma CPU específica, para que o processo unicamente rode em um core do processador.

---
+++ matador_cpu.cpp
@@ -0,0 +1,76 @@
+// UFRN-CT-DCA
+// Disciplina: Sistemas Operacionais
+// Programa matatdor.cpp
+// Sinais sã eventos gerados pelo Unix em resposta a algumas condições, como:
+//  erros, violação de memória, instrução ilegal, ...
+
+#include <signal.h> // definição dos sinais de interrupções
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h> // system()
+#include <sched.h>
+
+
+
+int  main()
+{
+  // o processo que vai matar
+
+  int meu_pid;
+  int pid_vitima, resposta;
+
+  meu_pid = getpid();
+    
+  
+ printf( "Eu sou um programa matador de programa meu PID é %d\n", meu_pid );
+ printf( "Digite o PID do processo \n");
+ scanf( "%d", &pid_vitima);
+ printf( "O que deseja fazer ? \n");
+ printf( "1 - Matar \n");
+ printf( "2 - Parar \n");
+ printf( "3 - Continuar \n");
+ printf( "4 - Alocar CPU \n");
+ scanf( "%d", &resposta);
+  
+ switch(resposta){
+
+ case 1: 
+ 	printf("\n Vou matar o processo %d \n", pid_vitima);
+ 	kill(pid_vitima, SIGKILL);
+ 	printf( "\nAgora posso morrer tranquilo.\n");
+	break;
+ case 2: 
+	printf("\n Vou parar o processo %d \n", pid_vitima);
+ 	kill(pid_vitima, SIGSTOP);
+ 	printf( "\n Processp parado\n");
+	break;
+ case 3:
+        printf("\n Vou Continuar o processo %d \n", pid_vitima);
+ 	kill(pid_vitima, SIGCONT);
+ 	printf( "\n Executando...\n");
+	break;
+ case 4:
+	int n_cpu;
+        printf("\n Vou alocar a CPU  %d \n", pid_vitima);
+        printf("\n Mas qual CPU vc quer o processo rode? Escolha '0' ou '1' \n");
+	scanf("%d",&n_cpu);
+ 	//kill(pid_vitima, SIGCONT);
+
+ 	printf( "\n Executando...\n");
+ 	cpu_set_t cpu;
+	CPU_ZERO(&cpu);
+	CPU_SET(n_cpu, &cpu);
+	sched_setaffinity (pid_vitima, sizeof(cpu_set_t), &cpu);
+	break;
+}
+
+
+ exit(0);
+
+}
+
+
+
+
+
+
